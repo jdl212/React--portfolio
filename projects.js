ed8e7c56aaae9fac95ad8abf9d19786337ba3c8d
jdl212 <68754578+jdl212@users.noreply.github.com>
2021-01-18 22:26:08 -0500

    modified project image
    updated project image

---
+++ projects.js
@@ -0,0 +1,57 @@
+export default
+{
+    name :'Josh Lerman',
+    email:'email@email.com',
+    resume: "./resume.pdf",
+    occupation: "Web Developer.",
+    aboutText: "Software Developer with a demonstrated history of working in the information technology and services industry. Skilled in Java, Spring, Python, SQL, NODE JS , HTML , JS . Strong information technology professional with a Bachelor's Degree focused in Computer Science",
+    aboutImage:'',
+    ShowAboutImage:false,
+    projects:[
+        {
+            id: 1,
+            title: 'Weather Application Details',
+            description: 'Weather Application Details',
+            imageSrc: "/img/projects/project-1.png",
+            url: 'https://github.com/jdl212/NewWeatherAPI'
+        },        {
+            id: 2,
+            title: 'Eat-Da-Burger!',
+            description: 'Eat-Da-Burger!',
+            imageSrc: "/img/projects/project-2.png",
+            url: 'https://github.com/jdl212/expressHandleBars'
+        },
+        {
+            id: 3,
+            title: 'Employee Management',
+            description: 'Employee Management',
+            imageSrc: "/img/projects/project-3.png",
+            url: 'https://github.com/jdl212/Employee-test.js'
+        },
+        {
+            id: 4,
+            title: 'Reverse Engineering UNIT 14',
+            description: 'Reverse Engineering UNIT 14',
+            imageSrc: "/img/projects/project-4.png",
+            url: 'https://github.com/jdl212/reverseEngineeringCode'
+        },
+        {
+            id: 5,
+            title: 'Github Portfolio',
+            description: 'Github Portfolio',
+            imageSrc: "/img/projects/project-5.png",
+            url: 'https://github.com/jdl212/student-bio'
+        },
+
+
+    ],
+    social: [
+
+        {   name:'Github',
+            url:'https://github.com/jdl212'},
+        {
+            name: 'Linkedin',
+            url: 'https://www.linkedin.com/'
+        }
+    ]
+}
